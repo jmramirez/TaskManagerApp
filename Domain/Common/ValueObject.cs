487179f120b2944707ab18e7a4f2ef21cbe8a4d9
Jose <josemramirez81@outlook.com>
2025-05-13 16:13:01 -0400

    Define generic base BaseEntity and ValueObject classes

---
+++ Domain/Common/ValueObject.cs
@@ -0,0 +1,20 @@
+namespace Domain.Common;
+
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if(obj == null || obj.GetType() != GetType())
+            return false;
+        
+        var other = (ValueObject)obj;
+
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+    
+    public override int GetHashCode() =>
+        GetEqualityComponents()
+            .Aggregate(1, (current, obj) => HashCode.Combine(current, obj?.GetHashCode() ?? 0));
+}
