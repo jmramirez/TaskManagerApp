d19ef6053d58fca1e276988fe39a62c1fb0b4319
Jose <josemramirez81@outlook.com>
2025-05-13 17:16:26 -0400

    Modify BaseEntity class to make it more reusable

--- Domain/Common/BaseEntity.cs
+++ Domain/Common/BaseEntity.cs
@@ -1,6 +1,30 @@
 namespace Domain.Common;
 
-public abstract class BaseEntity<TId>
+public abstract class BaseEntity<TId> where TId : notnull
 {
-    public TId Id { get; protected set; } = default!;
+    public TId Id { get; protected set; }
+
+    protected BaseEntity(TId id)
+    {
+        Id = id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if(obj is not BaseEntity<TId> other) return false;
+        return Id.Equals(other.Id);
+    }
+
+    public override int GetHashCode() => Id.GetHashCode();
+
+    public static bool operator ==(BaseEntity<TId>? left, BaseEntity<TId>? right)
+    {
+        return Equals(left, right);
+    }
+    
+    public static bool operator !=(BaseEntity<TId>? left, BaseEntity<TId>? right)
+    {
+        return !Equals(left, right);
+    }
+
 }
