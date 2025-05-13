cb81d3ec5419fa9409f14bc0bd2cd32634dfbcd7
Jose <josemramirez81@outlook.com>
2025-05-13 17:17:43 -0400

    Define TaskItem class

--- Domain/Tasks/TaskItem.cs
+++ Domain/Tasks/TaskItem.cs
@@ -1,6 +1,38 @@
+using Domain.Common;
+using Domain.Tasks.ValueObjects;
+
 namespace Domain.Tasks;
 
-public class TaskItem
+public sealed class TaskItem : BaseEntity<TaskId> 
 {
+    public TaskTitle Title { get;private set; }
+    public TaskDescription Description { get; private set; }
+    public DueDate DueDate { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public bool IsCompleted { get; private set; }
+
+    private TaskItem(TaskId id, TaskTitle title, TaskDescription description, DueDate dueDate, DateTime createdAt) : base(id)
+    {
+        Title = title;
+        Description = description;
+        DueDate = dueDate;
+        IsCompleted = false;
+        CreatedAt = createdAt;
+    }
+
+    public static Result<TaskItem> Create(TaskTitle title, TaskDescription description, DueDate dueDate,
+        DateTime createdAt)
+    {
+        var id = TaskId.New();
+        return Result<TaskItem>.Success(new TaskItem(id, title, description, dueDate, createdAt));
+    }
     
+    public void MarkCompleted() => IsCompleted = true;
+
+    public void Update(TaskTitle title, TaskDescription description, DueDate dueDate)
+    {
+        Title = title;
+        Description = description;
+        DueDate = dueDate;
+    }
 }
